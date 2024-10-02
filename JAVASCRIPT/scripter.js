03aa169790551596da90ac5d7f77bf6b45d1e058
jp12345q <verdad.194305@meycauayan.sti.edu.ph>
2024-10-02 16:27:17 +0800

    update script remove "/"

--- JAVASCRIPT/scripter.js
+++ JAVASCRIPT/scripter.js
@@ -1,10 +1,10 @@
-fetch('/HTML/main.html')
+fetch('HTML/main.html')
 .then(Response => Response.text())
 .then(data => {
     document.getElementById('example').innerHTML = data;
 });
 
-fetch('/HTML/header.html')
+fetch('HTML/header.html')
 .then(Response => Response.text())
 .then(data => {
     document.getElementById('headers').innerHTML = data;
